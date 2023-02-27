fa794a3d73059c8699fc087935dd7ac4a1d8e26a
ivofrancisco <mazivo.mun@gmail.com>
2023-02-27 13:51:24 +0000

    First Commit

---
+++ block.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:       Block
+ * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Requires at least: 5.7
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors + Ivo Francisco
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       block
+ *
+ * @package           if-blocks
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
+ */
+function if_blocks_block_block_init()
+{
+    register_block_type_from_metadata(__DIR__);
+}
+add_action('init', 'if_blocks_block_block_init');
